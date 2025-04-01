6d30e0c83f1072e9b3721b7b2dd939fe46179e2a
kalopsia21t <taras505paliy@gmail.com>
2025-04-02 02:02:23 +0300

    Fix header align

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}>Copyright 2025 © Wildsync</footer>;
+  return (
+    <footer className={styles.footer}>Copyright 2025 © Wildsync Kyiv</footer>
+  );
 }
