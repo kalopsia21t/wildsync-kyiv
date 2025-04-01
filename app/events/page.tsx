6a4d13bcfe3d24fcb5ed6d0dab9fb01d62d2b601
kalopsia21t <taras505paliy@gmail.com>
2025-04-02 01:00:15 +0300

    update Widget component

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,29 +3,32 @@ import styles from "@styles/events.module.css";
 
 const events = [
   {
-    title: "Event1",
-    img: "href1",
-    ticketsSrc: "otttry",
+    title: "Wildsync 15.02 at Mezzanine",
+    img: "/images/event2.jpeg",
+    actionSrc:
+      "https://ottry.com/events/wildsync?fbclid=PAZXh0bgNhZW0CMTEAAaaPOYWrXiMGNAe9DRTQOEgf85tIp7fH-e1JIpnpGIypjBh_tKeAjNAergQ_aem_qwdI_BJ6cA1FlGG42fQmvA",
+    action: "Buy Tickets",
   },
   {
-    title: "Event2",
-    img: "href1",
-    ticketsSrc: "otttry",
+    title: "Strictly - Connector EP",
+    img: "/images/connector.jpg",
+    actionSrc:
+      "https://scaredmoneyrecords.bandcamp.com/album/strictly-connector-ep",
+    action: "Buy",
   },
   {
-    title: "Event3",
-    img: "href1",
-    ticketsSrc: "otttry",
+    title: "Tunage w/ Maze of Death @ 20ft Radio - 05/04/2024",
+    img: "/images/mixcloud.jpg",
+    actionSrc:
+      "https://www.mixcloud.com/20ftradio/tunage-w-maze-of-death-20ft-radio-05042024/",
+    action: "Listen Mix",
   },
   {
-    title: "Event4",
-    img: "href1",
-    ticketsSrc: "otttry",
-  },
-  {
-    title: "Event5",
-    img: "href1",
-    ticketsSrc: "otttry",
+    title: "mazeofdeath at HVLV, Kyiv 28/07/23",
+    img: "/images/AJ2_005.jpg",
+    actionSrc:
+      "https://soundcloud.com/mazeofdeath/maze-of-death-amenjunkies-hvlv-290723",
+    action: "Listen Mix",
   },
 ];
 
@@ -33,7 +36,7 @@ export default function Events() {
   return (
     <div className={styles.event}>
       {events.map((event, index) => {
-        return <Widget key={`event-widget-${index}`} />;
+        return <Widget key={`event-widget-${index}`} {...event} />;
       })}
     </div>
   );
