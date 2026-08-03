eda02ea6d7c773773aa74c1c3029d4dc7e618280
kalopsia21t <taras505paliy@gmail.com>
2026-08-04 00:28:25 +0300

    update links and texts

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
+
 import styles from "./Header.module.css";
 
 import DropdownMenu from "@components/DropdownMenu";
 
+
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div>
+      <Link href="/">
         <Image src="/images/wildsync_white.png" alt="Wildsync" width={280} height={30}/>
-      </div>
+      </Link>
       <div>
         <DropdownMenu />
       </div>
