06767beffcb050054595b3096953c182322f5b43
kalopsia21t <taras505paliy@gmail.com>
2026-08-20 23:41:59 +0300

    type fix

--- utils/getEventBySlug.ts
+++ utils/getEventBySlug.ts
@@ -1,6 +1,8 @@
 import { GalleryEventT } from '@data/galleryevents';
 import { EventT } from '@data/events';
 
-export function getEventBySlug(events: GalleryEventT[] | EventT[], slug: string) {
-  return events.find((event: GalleryEventT | EventT) => event.slug === slug) || null;
+type EventRecord = GalleryEventT | EventT;
+
+export function getEventBySlug<T extends EventRecord>(events: T[], slug: string): T | null {
+  return events.find((event) => event.slug === slug) || null;
 }
